782183c6180a2987d373ceb6c1d0594484c7ae12
OSHAN JR <79368912+oshanjr@users.noreply.github.com>
2026-02-16 15:19:42 +0530

    feat: Implement initial food delivery app structure, including assets, screens, and data constants.

---
+++ app/(auth)/sign-up.tsx
@@ -0,0 +1,15 @@
+import { router } from 'expo-router'
+import { View, Text, Button } from 'react-native'
+
+
+const SignUp = () => {
+    return (
+        <View>
+            <Text>SignUp</Text>
+            <Button title="Sign Up" onPress={() => router.push("/sign-in")} />
+
+        </View>
+    )
+}
+
+export default SignUp
--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,25 @@
-import { Stack } from "expo-router";
+import { useFonts } from "expo-font";
+import { SplashScreen, Stack } from "expo-router";
+import { useEffect } from "react";
+
+import './global.css';
+
+SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
-  return <Stack />;
+  const [fontsLoaded, error] = useFonts({
+    "Quicksand-Regular": require("../assets/fonts/Quicksand-Regular.ttf"),
+    "Quicksand-Bold": require("../assets/fonts/Quicksand-Bold.ttf"),
+    "Quicksand-SemiBold": require("../assets/fonts/Quicksand-SemiBold.ttf"),
+    "Quicksand-Light": require("../assets/fonts/Quicksand-Light.ttf"),
+    "Quicksand-Medium": require("../assets/fonts/Quicksand-Medium.ttf"),
+  });
+
+  useEffect(() => {
+    if (error) throw error;
+    if (fontsLoaded) SplashScreen.hideAsync();
+
+  }, [fontsLoaded, error]);
+
+  return <Stack screenOptions={{ headerShown: false }} />;
 }
